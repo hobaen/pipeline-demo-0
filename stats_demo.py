c0d326009e58b6fba88dd254223ed97ac415243e
ERogers264 <earhonrogers@gmail.com>
2023-11-09 20:41:44 -0400

    tester

--- stats_demo.py
+++ stats_demo.py
@@ -12,19 +12,53 @@ def calculate_mean(numbers):
 
 
 
-def caLCulate_median(numbers):
-  
-    return None
-def calculate_mode(numbes):
-    return None
+def caLculate_median(numbers):
+    # Sort the list of numbers
+    sorted_numbers = sorted(numbers)
 
-def calculate_standard_deviation(nubers):
-    return None
+    # Find the middle index
+    middle_index = len(sorted_numbers) // 2
+
+    # Check if the list has an even or odd number of elements
+    if len(sorted_numbers) % 2 == 0:
+        # If even, take the average of the two middle elements
+        median = (sorted_numbers[middle_index - 1] + sorted_numbers[middle_index]) / 2
+    else:
+        # If odd, take the middle element
+        median = sorted_numbers[middle_index]
+
+    return median
+
+def calculate_mode(numbers):
+   frequency_table = {}
+    for num in numbers:
+        if num in frequency_table:
+            frequency_table[num] += 1
+        else:
+            frequency_table[num] = 1
+    if not numbers:
+        return None
+
+def calculate_standard_deviation(numbers):
+     # Calculate the mean
+    mean = sum(numbers) / len(numbers)
+
+    # Calculate the squared differences from the mean
+    squared_diff = [(x - mean) ** 2 for x in numbers]
+
+    # Calculate the variance
+    variance = sum(squared_diff) / len(numbers)
+
+    # Calculate the standard deviation
+    std_deviation = math.sqrt(variance)
+
+    return std_deviation
+    
 
 
 def calculate_range(numbers):
     return None
-def calculate_statistics(umbers):
+def calculate_statistics(numbers):
     descriptiveStats = {'mean': calculate_mean(numbers), 'median':calculate_median(numbers),'mode':calculate_mode(numbers),'standard_deviation': calculate_standard_deviation(numbers),'range':calculate_range(numbers)
     }
     return descriptiveStats
